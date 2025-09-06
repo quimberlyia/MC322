59ae04a632b5ed2f83ee6ababe9c8413e8f48bf5
Gabriel Lucena <gabriellucena.dev@gmail.com>
2025-09-05 21:55:37 -0300

    Add lucky, level up and equip weapon to hero

--- lab01/src/classes/Hero.java
+++ lab01/src/classes/Hero.java
@@ -3,12 +3,18 @@ package classes;
 
 public abstract class Hero extends Character {
 
-    public Hero(String name, int healthPoints, int strength) {
-        super(name, healthPoints, strength);
-    }
-
     private int level = 1;
     private int experience = 0;
+    private double lucky = 0;
+
+    public Hero(String name, int healthPoints, int strength, double lucky) {
+        super(name, healthPoints, strength);
+        this.lucky = lucky;
+    }
+
+    public double getLucky() {
+        return lucky;
+    }
 
     public int getLevel() {
         return level;
@@ -18,8 +24,24 @@ public abstract class Hero extends Character {
         return experience;
     }
 
+    public void setLucky(double lucky) {
+        this.lucky = lucky;
+    }
+
+    private void levelUp() {
+        this.level++;
+        this.lucky += 0.1;
+        addStrength(level * 3);
+        changeHealth(level * 5);
+        System.out.println(getName() + " leveled up to level " + level + "!");
+    }
+
     public void gainExperience(int exp) {
         this.experience += exp;
+        if (this.experience >= 100) {
+            levelUp();
+            this.experience = 0;
+        }
     }
 
     public abstract void useSpecialSkill(Character target);
@@ -27,5 +49,16 @@ public abstract class Hero extends Character {
     @Override
     public void showStatus() {
         System.out.println("The hero " + getName() + " is at level " + level + " with " + getHealthPoints() + " health points, " + getStrength() + " strength, and " + experience + " experience points.");
+    }
+
+    public void equipWeapon(Weapon weapon) {
+
+        if (weapon.getMinNivel() > this.level) {
+            System.out.println(getName() + " cannot equip " + weapon.getName() + " because they are not at the required level.");
+            return;
+        }
+
+        setWeapon(weapon);
+        System.out.println(getName() + " equipped " + weapon.getName() + "!");
     }
 }
