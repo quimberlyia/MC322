04d885c80d4a462bbb3449a9ea3e2abc2b27a929
Gabriel Lucena <gabriellucena.dev@gmail.com>
2025-09-13 19:22:14 -0300

    Implements main world mechanics

--- lab01/src/main.java
+++ lab01/src/main.java
@@ -1,94 +1,60 @@
 // Import the Player class from the Player.java file
-import classes.BuildScene;
+import classes.DungeonWorld;
 import classes.Hero;
 import classes.Monster;
-import classes.Scene;
+import classes.scenes.CombatScene;
 import classes.Weapon;
 import classes.heroes.Archer;
 import classes.heroes.Mage;
-import classes.monsters.Cockatrice;
-import classes.monsters.DemonKing;
-import classes.monsters.Lizardman;
-import classes.monsters.Dragon;
-import classes.monsters.Slime;
-import classes.monsters.Skeleton;
-import classes.Weapons.*;
+import interfaces.CombatAction;
 
 public class main {
     public static void main(String[] args) {
 
-        Weapon[] easyLoot = new Weapon[] { new Bow(), new Staff() };
-        Weapon[] mediumLoot = new Weapon[] { new SuperBow(), new WillowStaff() };
-        Weapon[] hardLoot = new Weapon[] { new ExtremeSuperBow(), new MoonStaff(), new Sword() };
+        // Implements Dungeon World
+        DungeonWorld dungeon = new DungeonWorld();
 
-        // Generates the game scenario
-        Scene[] scenes = BuildScene.generateScenes(3);
+        // Implements hero selection
+        Hero[] heroes = {
+            new Mage("Wizard of Oz", 200, 15, 50, 0.8),
+            new Archer("Artemis", 300, 10, 30, 0.8)
+        };
+        Hero hero = heroes[(int) (Math.random() * heroes.length)];
 
-        scenes[0].setEnvironment("Dungeon Entrance");
+        for (CombatScene scene : dungeon.getScenes()) {
 
-        scenes[0].setMonsters(new Monster[] { 
-            new Slime("Slime", 50, 5, 10, easyLoot),
-            new Skeleton("Skeleton", 70, 10, 15, easyLoot),
-            new Lizardman("Lizardman", 90, 15, 25, mediumLoot)
-        });
-
-        scenes[1].setEnvironment("Dark Hallway");
-
-        scenes[1].setMonsters(new Monster[] { 
-            new Dragon("Dragon", 150, 35, 80, hardLoot),
-            new Cockatrice("Cockatrice", 80, 30, 20, hardLoot)
-        });
-
-        scenes[2].setEnvironment("Demon King's Lair");
-
-        scenes[2].setMonsters(new Monster[] { 
-            new DemonKing("Demon King", 200, 50, 100, hardLoot)
-        });
-
-        // Initialize possible heroes
-        Mage mage = new Mage("Wizard of Oz", 200, 15, 50, 0.8);
-        Archer archer = new Archer("Artemis", 300, 10, 30, 0.8);
-
-        // Select one random hero
-        Hero[] heroes = { mage, archer };
-        Hero hero = heroes[(int) (Math.random() * 2)];
-        
-        System.out.println("The hero has entered the Castle of the Demon King to challenge the most evil creature in the world!");
-        
-        for (Scene scene : scenes) {
-            System.out.println("====================================================================================");
-            System.out.println("=============== The hero has entered the " + scene.getEnvironment() + "!");
-            System.out.println("====================================================================================\n");
+            scene.Start(hero);
             
-            hero.showStatus();
-
             for (Monster monster : scene.getMonsters()) {
                 System.out.println("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
                 System.out.println(">>>>> The " + monster.getName() + " is ready to slaughter the hero!");
                 System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
-                monster.showStatus();
 
-                System.out.println("\n----------------------------------------------------");
-                while (hero.getHealthPoints() > 0 && monster.getHealthPoints() > 0) {
-                    hero.attack(monster);
-                    if (monster.getHealthPoints() > 0) {
-                        monster.attack(hero);
-                    }
+                while (hero.isAlive() && monster.isAlive()) {
+                    hero.showStatus();
+                    monster.showStatus();
+
+                    CombatAction heroAction = hero.chooseAction(monster);
+                    CombatAction monsterAction = monster.chooseAction(hero);
+
+                    // Execute actions
+                    heroAction.execute(hero, monster);
+
+                    if (monster.isAlive())
+                        monsterAction.execute(monster, hero);
                 }
 
-                if (hero.getHealthPoints() <= 0) {
-                    
+                if (!hero.isAlive()) {
                     System.out.println("----------------------------------------------------\n");
                     System.out.println("The hero " + hero.getName() + " has been defeated! Game Over.");
                     return;
                 }
 
                 System.out.println("The hero " + hero.getName() + " has defeated the " + monster.getName() + "!");
-                
-                hero.gainExperience(monster.getExperience());
 
+                hero.gainExperience(monster.getExperience());
                 if (Math.random() < hero.getLucky()) {
-                    Weapon loot = monster.dropLoot();
+                    Weapon loot = (Weapon) monster.dropLoot();
                     System.out.println("Due to luck, " + hero.getName() + " found the " + loot.getName() + " on the " + monster.getName() + "!");
                     
                     if (hero.getWeapon().getDamage() < loot.getDamage()) {
@@ -97,11 +63,10 @@ public class main {
                         System.out.println("But the " + loot.getName() + " is not better than the current weapon.");
                     }
                 }
-
             }
             System.out.println("----------------------------------------------------");
 
-            System.out.println("\n>>>>> The hero has cleared the " + scene.getEnvironment() + "!\n");
+            System.out.println("\n>>>>> The hero has cleared the floor!\n");
         }
 
         System.out.println("\n" + hero.getName() + " has defeated the Demon King and completed the dungeon!");
