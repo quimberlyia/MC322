6da900ae186e5a7f89002b60f0006dc534c47408
Gabriel Lucena <gabriellucena.dev@gmail.com>
2025-09-20 21:34:59 -0300

    Difficulty name file

--- com.rpglab.dungeonadventure/enums/Difficulty.java
+++ com.rpglab.dungeonadventure/enums/Difficulty.java
@@ -1,6 +1,6 @@
 package enums;
 
-public enum Dificult {
+public enum Difficulty {
     EASY(0.8, 0.8), // Multiplicadores para dificuldade fácil
     MEDIUM(1.0, 1.0),
     HARD(1.3, 1.2);
@@ -8,7 +8,7 @@ public enum Dificult {
     private final double lifeMult; // Multiplicador de vida
     private final double damageMult; // Multiplicador de dano
 
-    Dificult(double lifeMult, double damageMult) {
+    Difficulty(double lifeMult, double damageMult) {
         this.lifeMult = lifeMult; // Inicializa o multiplicador de vida
         this.damageMult = damageMult;  // Inicializa o multiplicador de dano
     }
